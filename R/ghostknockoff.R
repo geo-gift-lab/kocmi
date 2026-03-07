552096b6aeadb30aeeb75ea0c080f67b15b283d2
SpatLyu <lyu.geosocial@gmail.com>
2026-03-08 00:00:01 +0800

    draft ghostknockoff operation

--- R/ghostknockoff.R
+++ R/ghostknockoff.R
@@ -258,15 +258,11 @@ is_posdef = \(A, tol=1e-9) {
   if (p < 500) {
     lambda_min = min(eigen(A)$values)
   } else {
-    oldw = getOption("warn")
-    #options(warn = -1)
-    lambda_min = suppressWarnings{
-      RSpectra::eigs(A, 1, which="SM",
-                     opts=list(retvec = FALSE, maxitr=100, tol))$values
-    }
-      )
-    options(warn = oldw)
-    if( length(lambda_min)==0 ) {
+    lambda_min = suppressWarnings({
+      RSpectra::eigs(A, 1, which = "SM",
+                     opts = list(retvec = FALSE, maxitr=100, tol))$values
+    })
+    if (length(lambda_min) == 0) {
       # RSpectra::eigs did not converge. Using eigen instead.
       lambda_min = min(eigen(A)$values)
     }
