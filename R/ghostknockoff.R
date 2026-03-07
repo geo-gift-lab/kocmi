ed259389858243a7acc0de367100ffc95876d9b7
SpatLyu <lyu.geosocial@gmail.com>
2026-03-08 00:00:01 +0800

    draft ghostknockoff operation

--- R/ghostknockoff.R
+++ R/ghostknockoff.R
@@ -59,16 +59,16 @@ GhostKnockoff.prelim = \(cor.G, M=5, method="asdp", max.size=500){
               permute.index = permute.index, M = M))
 }
 
-create.solve_sdp_M <- function(Sigma, M=1, gaptol=1e-6, maxit=1000, verbose=FALSE) {
+create.solve_sdp_M = \(Sigma, M=1, gaptol=1e-6, maxit=1000, verbose=FALSE) {
   # Check that covariance matrix is symmetric
-  stopifnot(isSymmetric(Sigma))
+  stopifnot(Matrix::isSymmetric(Sigma))
   # Convert the covariance matrix to a correlation matrix
-  G = cov2cor(Sigma)
+  G = Matrix::cov2cor(Sigma)
   p = dim(G)[1]
 
   # Check that the input matrix is positive-definite
   if (!is_posdef(G)) {
-    warning('The covariance matrix is not positive-definite: knockoffs may not have power.', immediate.=T)
+    warning("The covariance matrix is not positive-definite: knockoffs may not have power.", immediate.=T)
   }
 
   # Convert problem for SCS
@@ -83,14 +83,14 @@ create.solve_sdp_M <- function(Sigma, M=1, gaptol=1e-6, maxit=1000, verbose=FALS
   d_As = c(diag(p))
   As = Matrix::Diagonal(length(d_As), x=d_As)
   As = As[which(Matrix::rowSums(As) > 0),]
-  Cs = c((M+1)/M*G) ##change from 2 to (M+1)/M
+  Cs = c((M+1)/M*G) #change from 2 to (M+1)/M
 
   # Assemble constraints and cones
   A = cbind(Al1,Al2,As)
   C = matrix(c(Cl1,Cl2,Cs),1)
   K=NULL
   K$s=p
-  K$l=2*p #not sure if it should be changed - may be not as it is the dimention of the linear part.
+  K$l=2*p #not sure if it should be changed - may be not as it is the dimension of the linear part.
 
   # Objective
   b = rep(1,p)
@@ -107,21 +107,21 @@ create.solve_sdp_M <- function(Sigma, M=1, gaptol=1e-6, maxit=1000, verbose=FALS
   if(verbose) cat("done. \n")
 
   # Check whether the solution is feasible
-  if( ! identical(sol$STATS$stype,"PDFeasible")) {
+  if(!identical(sol$STATS$stype,"PDFeasible")) {
     warning('The SDP solver returned a non-feasible solution. Knockoffs may lose power.')
   }
 
   # Clip solution to correct numerical errors (domain)
   s = sol$y
-  s[s<0]=0
-  s[s>1]=1
+  s[s<0] = 0
+  s[s>1] = 1
 
   # Compensate for numerical errors (feasibility)
   if(verbose) cat("Verifying that the solution is correct ... ")
   psd = 0
   s_eps = 1e-8
   while ((psd==0) & (s_eps<=0.1)) {
-    if (is_posdef((M+1)/M*G-diag(s*(1-s_eps),length(s)),tol=1e-9)) { ##change from 2 to (M+1)/M
+    if (is_posdef((M+1)/M*G-diag(s*(1-s_eps),length(s)),tol=1e-9)) { #change from 2 to (M+1)/M
       psd  = 1
     }
     else {
@@ -129,7 +129,7 @@ create.solve_sdp_M <- function(Sigma, M=1, gaptol=1e-6, maxit=1000, verbose=FALS
     }
   }
   s = s*(1-s_eps)
-  s[s<0]=0
+  s[s<0] = 0
   if(verbose) cat("done. \n")
 
   # Verify that the solution is correct
