dac31adeeac38e2b8b111f5d81a7fac9deba6eb2
SpatLyu <lyu.geosocial@gmail.com>
2026-04-13 00:00:01 +0800

    new kocmi single function

--- R/kocmi.R
+++ R/kocmi.R
@@ -1,12 +1,14 @@
-kocmi_single = \(data, target, agent, conds, type = c("cont", "disc"),
+kocmi_single = \(data, target, regulator, conds, type = c("cont", "disc"),
                  monte = 40, nboots = 1e4, k = 3, threads = 1, seed = 42,
                  base = exp(1), method = "equal", contain_null = TRUE) {
   type = match.arg(type)
   mat = infoxtr:::.convert2mat(data, type)
-  knockoff = construct_knockoff(mat, agent, conds, monte, seed)
+  knockoff = construct_knockoff(mat, regulator, conds, monte, seed)
   null_knockoff = NULL
-  if (contain_null) null_knockoff = construct_knockoff(mat, agent, c(target,conds), monte, seed)
+  if (contain_null) {
+    null_knockoff = construct_knockoff(mat, regulator, c(target,conds), monte, seed)
+  }
   return(infoxtr:::RcppKOCMI(
-            mat, target, agent, conds, knockoff, null_knockoff, type,
+            mat, target, regulator, conds, knockoff, null_knockoff, type,
             nboots, k, 0, threads, seed, base, method, contain_null))
 }
