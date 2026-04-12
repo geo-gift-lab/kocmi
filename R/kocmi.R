418622a3ef01ad821b4b4287f45a465264022171
SpatLyu <lyu.geosocial@gmail.com>
2026-04-13 00:00:01 +0800

    new kocmi single function

---
+++ R/kocmi.R
@@ -0,0 +1,10 @@
+kocmi_single = \(data, target, agent, conds, type = c("cont", "disc"),
+                 nboots = 1e4, k = 3, threads = 1, seed = 42,
+                 base = exp(1), method = "equal", contain_null = TRUE) {
+  type = match.arg(type)
+  mat = .convert2mat(data, type)
+  knockoff = .convert2mat(knockoff, contain_type = FALSE)
+  null_knockoff = .convert2mat(null_knockoff, contain_type = FALSE)
+  return(RcppKOCMI(mat, target, agent, conds, knockoff, null_knockoff, type,
+                   nboots, k, 0, threads, seed, base, method, contain_null))
+}
