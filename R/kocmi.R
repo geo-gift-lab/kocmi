56edc50d851769cc3b4551b1b7fd4a0af9872eac
SpatLyu <lyu.geosocial@gmail.com>
2026-04-13 00:00:01 +0800

    draft kocmi network version

--- R/kocmi.R
+++ R/kocmi.R
@@ -30,25 +30,35 @@ kocmi_single = \(data, target, regulator, conds, type = c("cont", "disc"),
                  base = exp(1), method = "equal", contain_null = TRUE) {
   type = match.arg(type)
   mat = infoxtr:::.convert2mat(data, type)
+
   knockoff = construct_knockoff(mat, regulator, conds, monte, seed)
+
   null_knockoff = NULL
   if (contain_null) {
     null_knockoff = construct_knockoff(mat, regulator, c(target,conds), monte, seed)
   }
+
   return(infoxtr:::RcppKOCMI(
             mat, target, regulator, conds, knockoff, null_knockoff, type,
             nboots, k, 0, threads, seed, base, method, contain_null))
 }
 
-kocmi_net = \(data, vars, type = c("cont", "disc"), monte = 40, nboots = 1e4, k = 3,
-              threads = 1, seed = 42, base = exp(1), method = "equal", contain_null = TRUE) {
+kocmi_net = \(data, vars, type = c("cont", "disc"), monte = 40, nboots = 1e4, k = 3, threads = 1,
+              seed = 42, base = exp(1), method = "equal", contain_null = TRUE, verbose = FALSE) {
   type = match.arg(type)
   mat = infoxtr:::.convert2mat(data[,vars,drop = FALSE], type)
   new_vars = seq_along(vars)
-  knockoff = construct_knockoff(mat, regulator, conds, monte, seed)
+
   null_knockoff = NULL
   if (contain_null) {
     null_knockoff = x_knockoff(mat, monte, seed)
+  }
+
+  var_pairs = utils::combn(1:5, 2, simplify = FALSE)
+  if (verbose) txtpb = utils::txtProgressBar(min = 1, max = length(var_pairs))
+  for (v in var_pairs) {
+    knockoff = construct_knockoff(mat, regulator, conds, monte, seed)
+
   }
   return(infoxtr:::RcppKOCMI(
     mat, target, regulator, conds, knockoff, null_knockoff, type,
