537df8dd0c00a78f335bfb85248106d72d306476
SpatLyu <lyu.geosocial@gmail.com>
2026-04-13 00:00:01 +0800

    draft kocmi network version

--- R/kocmi.R
+++ R/kocmi.R
@@ -39,3 +39,17 @@ kocmi_single = \(data, target, regulator, conds, type = c("cont", "disc"),
             mat, target, regulator, conds, knockoff, null_knockoff, type,
             nboots, k, 0, threads, seed, base, method, contain_null))
 }
+
+kocmi_net = \(data, vars, type = c("cont", "disc"), monte = 40, nboots = 1e4, k = 3,
+              threads = 1, seed = 42, base = exp(1), method = "equal", contain_null = TRUE) {
+  type = match.arg(type)
+  mat = infoxtr:::.convert2mat(data[,vars,drop = FALSE], type)
+  knockoff = construct_knockoff(mat, regulator, conds, monte, seed)
+  null_knockoff = NULL
+  if (contain_null) {
+    null_knockoff = construct_knockoff(mat, regulator, c(target,conds), monte, seed)
+  }
+  return(infoxtr:::RcppKOCMI(
+    mat, target, regulator, conds, knockoff, null_knockoff, type,
+    nboots, k, 0, threads, seed, base, method, contain_null))
+}
